f48a72ac7dcf52cec53f32a927324495af58bd6d
Tedzury <asdrogachev@gmail.com>
2023-12-05 21:39:45 +0300

    feat: add and configured eslint and prettier with plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,69 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'tailwind.config.js', 'postcss.config.js', 'prettier.config.js'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'react-refresh', '@typescript-eslint', 'simple-import-sort', 'import', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
   },
-}
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      react: {
+        version: 'detect',
+      },
+      node: true,
+    },
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
