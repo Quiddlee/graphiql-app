ede12264e37d5c9279dc75a169eccfffd00325b1
Tedzury <asdrogachev@gmail.com>
2023-12-09 10:00:55 +0300

    feat: add setup and libs for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        readex_pro: ['Readex Pro', 'sans-serif'],
+        jetbrains_mono: ['JetBrains Mono', 'sans-serif']
+      },
+      transitionTimingFunction: {
+        bounce: 'cubic-bezier(.25,1.55,.65,.97)',
+      },
+      height: {
+        screen: '100dvh',
+      },
+      colors: {
+        primary: 'var(--md-sys-color-primary)',
+      }
+    },
   },
   plugins: [],
 }
