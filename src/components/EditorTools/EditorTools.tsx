f5c5f03c61eed17d31f49bab4c1bca7f409b4adc
Bogdan <bogdanscherbinadev@gmail.com>
2023-12-15 11:52:59 +0200

    refactor: change editor styles

--- src/components/EditorTools/EditorTools.tsx
+++ src/components/EditorTools/EditorTools.tsx
@@ -3,6 +3,8 @@ import { useRef, useState } from 'react';
 import { MdTabs } from '@material/web/all';
 
 import EditorToolsField from '@components/EditorTools/ui/EditorToolsField';
+import Icon from '@shared/ui/Icon';
+import IconButton from '@shared/ui/IconButton';
 import PrimaryTab from '@shared/ui/PrimaryTab';
 import Tabs from '@shared/ui/Tabs';
 
@@ -19,12 +21,19 @@ const EditorTools = () => {
   };
 
   return (
-    <article className="grid gap-7 rounded-4xl bg-surface-container py-4 pl-[26px] pr-4">
-      <Tabs onchange={handleTabSwitch} ref={tabsRef}>
-        <PrimaryTab>Variables</PrimaryTab>
-        <PrimaryTab>Headers</PrimaryTab>
-      </Tabs>
-      <EditorToolsField isVariablesTab={isVariablesTab} />
+    <article className="grid gap-7 rounded-4xl bg-surface-container py-4">
+      <header className="flex w-full justify-between border-b-2 border-outline-variant pl-7 pr-4">
+        <Tabs className="w-[360px] rounded-full [&::part(divider)]:hidden" onchange={handleTabSwitch} ref={tabsRef}>
+          <PrimaryTab>Variables</PrimaryTab>
+          <PrimaryTab>Headers</PrimaryTab>
+        </Tabs>
+        <IconButton>
+          <Icon>expand_more</Icon>
+        </IconButton>
+      </header>
+      <div className="pl-7 pr-4">
+        <EditorToolsField isVariablesTab={isVariablesTab} />
+      </div>
     </article>
   );
 };
