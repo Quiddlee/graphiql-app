0582d4471c357a03521f461c059db5167b742144
Bogdan <bogdanscherbinadev@gmail.com>
2023-12-15 11:18:57 +0200

    refactor: code rearrange

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,12 +2,31 @@ import { SyntheticEvent, useState } from 'react';
 
 import useEditorSize from '@components/Editor/lib/hooks/useEditorSize';
 import Controls from '@components/Editor/ui/Controls';
-import LineNumbers from '@components/Editor/ui/LineNumbers';
-import TextArea from '@components/Editor/ui/TextArea';
+import EditorField from '@components/Editor/ui/EditorField';
+
+const DEFAULT_VALUE = `# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that start
+# with a # are ignored.
+#
+# An example GraphQL query might look like:
+#
+# {
+#   field(arg: "value") {
+#   subField
+# }
+`;
 
 const Editor = () => {
   const { editorRef, size } = useEditorSize();
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(DEFAULT_VALUE);
 
   const handleChange = (e: SyntheticEvent) => {
     const { value: textAreaValue } = e.target as HTMLTextAreaElement;
@@ -17,10 +36,9 @@ const Editor = () => {
   return (
     <article
       ref={editorRef}
-      className="body-large flex h-full w-full gap-4 overflow-clip rounded-4xl bg-surface-container py-7 pl-7 pr-4 font-jetbrains_mono"
+      className="flex h-full w-full gap-4 overflow-clip rounded-4xl bg-surface-container pl-7 pr-4 pt-7"
     >
-      <LineNumbers size={size} />
-      <TextArea value={value} onChange={handleChange} />
+      <EditorField size={size} value={value} onChange={handleChange} />
       <Controls editorValue={value} />
     </article>
   );
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ import Nav from '@components/Nav/Nav';
 
 const MainLayout = () => {
   return (
-    <main className="grid h-screen grid-cols-[384px_1fr_1fr] grid-rows-[80px_1fr] px-4 pb-4">
+    <main className="grid h-screen grid-cols-[384px_1fr_.5fr] grid-rows-[80px_1fr] px-4 pb-4">
       <Header />
       <Nav />
       <section className="col-start-2 row-start-2 row-end-4">
