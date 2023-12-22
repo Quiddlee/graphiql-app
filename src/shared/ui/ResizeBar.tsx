ee6cfd785f593f256c9f556e336f0d09ab9a97d8
Bogdan <bogdanscherbinadev@gmail.com>
2023-12-22 18:50:29 +0200

    fix: resize bar to be fixed size

--- src/shared/ui/ResizeBar.tsx
+++ src/shared/ui/ResizeBar.tsx
@@ -16,8 +16,8 @@ const ResizeBar: FC<ResizeBarProps> = ({ className, direction = 'vertical', ...p
         'rounded-full before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-1.5 before:w-1.5 before:rounded-full before:transition-all before:delay-200 hover:cursor-grab before:hover:bg-primary before:hover:delay-0 active:cursor-grabbing before:active:bg-primary before:active:duration-100 before:active:ease-bounce',
         className,
         {
-          'h-4 w-full before:active:w-1/6': direction === 'vertical',
-          'h-full w-4 before:active:h-1/6': direction === 'horizontal',
+          'h-4 w-full before:active:w-24': direction === 'vertical',
+          'h-full w-4 before:active:h-24': direction === 'horizontal',
         },
       )}
     />
