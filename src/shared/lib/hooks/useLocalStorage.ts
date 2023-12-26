5e1897b5598ffba0e09600a3feb61919ceb89bf5
Bogdan <bogdanscherbinadev@gmail.com>
2023-12-26 18:19:35 +0200

    refactor: update local storage hook types

--- src/shared/lib/hooks/useLocalStorage.ts
+++ src/shared/lib/hooks/useLocalStorage.ts
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
  * @param {string | number | boolean} val - The value to be stored.
  * @returns {Function} A function that retrieves the stored value from local storage.
  */
-function useLocalStorage(key: string, val?: string | number | boolean) {
+function useLocalStorage(key: string, val?: string | number | boolean | Record<string | number, string>[]) {
 	useEffect(() => {
 		if (val !== undefined) localStorage.setItem(key, JSON.stringify(val));
 	}, [key, val]);
