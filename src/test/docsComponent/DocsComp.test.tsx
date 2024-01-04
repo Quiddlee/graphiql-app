ace8e9300fb24b03f1dd746d6cf5817429332645
Tedzury <asdrogachev@gmail.com>
2024-01-04 19:00:29 +0300

    test: attempt to figure out what is wrong with tests#2

--- src/test/docsComponent/DocsComp.test.tsx
+++ src/test/docsComponent/DocsComp.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import App from '@/app/App';
@@ -19,54 +19,54 @@ describe('Testing for docs component', () => {
   //     await screen.findByText('A GraphQL schema provides a root type for each kind of operation.'),
   //   ).toBeInTheDocument();
   // });
-  it('Should close docs section after clicking on overlay', async () => {
-    render(<App />);
-    const showDocsBtn = screen.getByTestId('show_docs');
-    expect(screen.queryByTestId('overlay')).toBeNull();
-    expect(screen.queryByText('Docs')).toBeNull();
-    expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation')).toBeNull();
-    await act(async () => {
-      fireEvent.click(showDocsBtn);
-    });
-    const overlay = await screen.findByTestId('overlay');
-    expect(overlay).toBeInTheDocument();
-    expect(await screen.findByText('Docs')).toBeInTheDocument();
-    expect(
-      await screen.findByText('A GraphQL schema provides a root type for each kind of operation.'),
-    ).toBeInTheDocument();
-    await act(async () => {
-      fireEvent.click(overlay);
-    });
-    waitForElementToBeRemoved(() => {
-      expect(overlay).toBeNull();
-      expect(screen.queryByText('Docs')).toBeNull();
-      expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation.')).toBeNull();
-    }).catch(() => {});
-  });
-  it('Should close docs section after clicking on close docs button', async () => {
-    render(<App />);
-    const showDocsBtn = screen.getByTestId('show_docs');
-    expect(screen.queryByTestId('overlay')).toBeNull();
-    expect(screen.queryByText('Docs')).toBeNull();
-    expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation')).toBeNull();
-    await act(async () => {
-      fireEvent.click(showDocsBtn);
-    });
-    const closeDocsBtn = await screen.findByText('closeDocs');
-    expect(await screen.findByTestId('overlay')).toBeInTheDocument();
-    expect(await screen.findByText('Docs')).toBeInTheDocument();
-    expect(
-      await screen.findByText('A GraphQL schema provides a root type for each kind of operation.'),
-    ).toBeInTheDocument();
-    await act(async () => {
-      fireEvent.click(closeDocsBtn);
-    });
-    waitForElementToBeRemoved(() => {
-      expect(screen.queryByTestId('overlay')).toBeNull();
-      expect(screen.queryByText('Docs')).toBeNull();
-      expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation.')).toBeNull();
-    }).catch(() => {});
-  });
+  // it('Should close docs section after clicking on overlay', async () => {
+  //   render(<App />);
+  //   const showDocsBtn = screen.getByTestId('show_docs');
+  //   expect(screen.queryByTestId('overlay')).toBeNull();
+  //   expect(screen.queryByText('Docs')).toBeNull();
+  //   expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation')).toBeNull();
+  //   await act(async () => {
+  //     fireEvent.click(showDocsBtn);
+  //   });
+  //   const overlay = await screen.findByTestId('overlay');
+  //   expect(overlay).toBeInTheDocument();
+  //   expect(await screen.findByText('Docs')).toBeInTheDocument();
+  //   expect(
+  //     await screen.findByText('A GraphQL schema provides a root type for each kind of operation.'),
+  //   ).toBeInTheDocument();
+  //   await act(async () => {
+  //     fireEvent.click(overlay);
+  //   });
+  //   waitForElementToBeRemoved(() => {
+  //     expect(overlay).toBeNull();
+  //     expect(screen.queryByText('Docs')).toBeNull();
+  //     expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation.')).toBeNull();
+  //   }).catch(() => {});
+  // });
+  // it('Should close docs section after clicking on close docs button', async () => {
+  //   render(<App />);
+  //   const showDocsBtn = screen.getByTestId('show_docs');
+  //   expect(screen.queryByTestId('overlay')).toBeNull();
+  //   expect(screen.queryByText('Docs')).toBeNull();
+  //   expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation')).toBeNull();
+  //   await act(async () => {
+  //     fireEvent.click(showDocsBtn);
+  //   });
+  //   const closeDocsBtn = await screen.findByText('closeDocs');
+  //   expect(await screen.findByTestId('overlay')).toBeInTheDocument();
+  //   expect(await screen.findByText('Docs')).toBeInTheDocument();
+  //   expect(
+  //     await screen.findByText('A GraphQL schema provides a root type for each kind of operation.'),
+  //   ).toBeInTheDocument();
+  //   await act(async () => {
+  //     fireEvent.click(closeDocsBtn);
+  //   });
+  //   waitForElementToBeRemoved(() => {
+  //     expect(screen.queryByTestId('overlay')).toBeNull();
+  //     expect(screen.queryByText('Docs')).toBeNull();
+  //     expect(screen.queryByText('A GraphQL schema provides a root type for each kind of operation.')).toBeNull();
+  //   }).catch(() => {});
+  // });
   it('Should navigate and display info about proper type after cliking on that type', async () => {
     render(<App />);
     const showDocsBtn = screen.getByTestId('show_docs');
