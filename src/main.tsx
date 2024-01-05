e77f9419d60bcb635e12ca30875c14246088a50d
Tedzury <asdrogachev@gmail.com>
2024-01-05 11:55:27 +0300

    feat: add ErrorBoundary and ErrorFallback components

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/app/App';
 
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import ErrorFallback from './components/ErrorFallback/ErrorFallback';
 import initFirebaseApp from './firebase';
 
 import '@/styles/index.css';
@@ -14,6 +16,8 @@ initFirebaseApp();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorFallback />}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
