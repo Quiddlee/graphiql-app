ce99259985f6c863c33863fbe6e2d347bf798276
Tedzury <asdrogachev@gmail.com>
2024-01-06 15:11:57 +0300

    feat: modified test coverage config

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,28 @@ export default defineConfig({
 			thresholds: {
 				statements: 50,
 			},
+			exclude: [
+				'coverage/**',
+				'dist/**',
+				'**/[.]**',
+				'packages/*/test?(s)/**',
+				'**/*.d.ts',
+				'**/virtual:*',
+				'**/__x00__*',
+				'**/\x00*',
+				'cypress/**',
+				'test?(s)/**',
+				'test?(-*).?(c|m)[jt]s?(x)',
+				'**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+				'**/__tests__/**',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,tailwind,postcss}.config.*',
+				'**/vitest.{workspace,projects}.[jt]s?(on)',
+				'**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+				'**/src/firebase.ts',
+				'**/src/main.tsx',
+			]
 		},
+		include: ['**/*.{test,spec}.[t]s?(x)'],
+		exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*']
 	},
 });
